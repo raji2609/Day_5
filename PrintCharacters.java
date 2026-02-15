f3910e87c17eb7fde018a4eb67b88d57c3881823
raji2609 <raji1998.k@gmail.com>
2026-02-15 22:30:40 +0530

    Add files via upload

---
+++ PrintCharacters.java
@@ -0,0 +1,9 @@
+public class PrintCharacters {
+    public static void main(String[] args) {
+        char[] letters = {'A', 'B', 'C', 'D', 'E'};
+
+        for (char ch : letters) {
+            System.out.print(ch + " ");
+        }
+    }
+}
